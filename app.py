279c9070204091e38a5a621e4739f743fda1b678
CyberPratik <karandesonu3@gmail.com>
2026-04-23 04:43:37 +0000

    Added phone input in form

--- app.py
+++ app.py
@@ -7,12 +7,10 @@ def register():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
+        phone = request.form['phone']
         event = request.form['event']
-
-        return f"<h2>Thank you {name} for registering for {event}!</h2><p>Confirmation sent to {email}</p>"
-
+        return f"<h2>Thank you {name} for registering for {event}!</h2> <p> Name: {name}<br> Email: {email}<br> Phone: {phone}</p>"
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0',port=5000)
